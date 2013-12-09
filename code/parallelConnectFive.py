f46519b026c830264a58f03f5a5f0c1316b5ba07
bksim <simpavid@gmail.com>
2013-12-08 20:43:40 -0500

    modified spiral with mods

--- code/parallelConnectFive.py
+++ code/parallelConnectFive.py
@@ -190,12 +190,25 @@ if __name__ == '__main__':
             for i in range(side_length):
                 spiral.append((spiral[-1][0]+f, spiral[-1][1]))
             for j in range(side_length):
-    	        spiral.append((spiral[-1][0], spiral[-1][1]+f))
+                spiral.append((spiral[-1][0], spiral[-1][1]+f))
             side_length+=1
         for i in range(1, int(size)):
             spiral.append((i, 0))
 
-        gameState = ConnectFiveGameState(clean_board, 1, moveOrdering=spiral)
+        # reorder the spiral!
+        modified_spiral = {}
+        num_processors = 8
+        for i, action in enumerate(spiral):
+            if i % num_processors not in modified_spiral.keys():
+                modified_spiral[i % num_processors] = [action]
+            else:
+                modified_spiral[i % num_processors].append(action)
+
+        mod_spiral = []
+        for key in modified_spiral.keys():
+            mod_spiral = mod_spiral + modified_spiral[key]
+
+        gameState = ConnectFiveGameState(clean_board, 1, moveOrdering=mod_spiral)
 
         boardGraphics = ConnectFiveGraphics(gameState, comm=comm, activateAI=True)
     else:
